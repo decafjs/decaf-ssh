3d162814b6bdaabd546712da99c77db824144e1c
Michael Schwartz <mykesx@gmail.com>
2014-09-01 13:05:50 -0700

    initial commit

---
+++ REAME.md
@@ -0,0 +1,5 @@
+SSH2
+====
+
+This code is heavily inspired by the SSH code in the mostly unsupported Helma project (http://helma.org).
+
---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mschwartz
+ * Date: 6/26/13
+ * Time: 2:31 PM
+ */
+
+/*global exports, require, decaf */
+
+decaf.extend(exports, {
+    Connection: require('lib/Connection').Connection
+});
